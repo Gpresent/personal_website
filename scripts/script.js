aa1f4e1638b5108e624d1760c864cfc6848d39ea
gpresent <gpresent@tamu.edu>
2023-09-14 18:33:48 -0500

    Porting changes over from other repo. Final website for Project 1.

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,25 @@
 // Define a boolean variable to track the current theme
-let isTheme1 : boolean = true;
-const themeLink = document.getElementById('theme-link') as HTMLLinkElement;
+var isTheme1 = true;
+var themeLink = document.getElementById('theme-link');
 if (sessionStorage.getItem('theme-one') == 'false') {
     themeLink.href = 'css/style2.css';
 }
-
-
 // Function to toggle the theme
 function toggleTheme() {
-    
     // Toggle the theme by changing the href attribute of the link element
     if (isTheme1) {
         themeLink.href = 'css/style2.css';
         sessionStorage.setItem('theme-one', 'false');
-    } else {
+    }
+    else {
         themeLink.href = 'css/style.css';
         sessionStorage.setItem('theme-one', 'true');
     }
-
     // Update the theme toggle state
     isTheme1 = !isTheme1;
 }
-
 // Add a click event listener to the button
-const themeToggleBtn = document.getElementById("theme-toggle") as HTMLElement;
+var themeToggleBtn = document.getElementById("theme-toggle");
 if (themeToggleBtn) {
     themeToggleBtn.addEventListener('click', toggleTheme);
 }
